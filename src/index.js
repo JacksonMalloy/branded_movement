4ab3cf56a7e9f04f4a6bda63b9e9c579bc254b10
Jackson Malloy <jacksmalloy@gmail.com>
2019-07-29 16:38:41 -0700

    testing again

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Triangles.css';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './scss/App.scss';
+import './Triangles.css';
 
 import ScrollToTop from './components/ScrollToTop';
 
